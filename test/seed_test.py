d78b98f666ca83ad7e7345e3ba92ffb3955d3bd7
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2021-06-29 18:53:08 +0200

    cleaner seed test

--- test/seed_test.py
+++ test/seed_test.py
@@ -18,10 +18,14 @@ def test_seed_reach():
     for action in actions:
         state, _, _, _ = env.step(action)
     # check somes values from the final state
-    # value are rounded : depending on the os, the result is not stricly the same
-    assert round(state["observation"][2], 8) == 0.1026114
-    assert round(state["achieved_goal"][1], 8) == 0.13984261
-    assert round(state["desired_goal"][0], 8) == 0.0063933
+    expected_state = {
+        "observation": np.array([-0.00562165, 0.13984261, 0.1026114, 0.57252472, 0.16387926, 0.35602099]),
+        "achieved_goal": np.array([-0.00562165, 0.13984261, 0.1026114]),
+        "desired_goal": np.array([0.0063933, -0.0785642, 0.26941446]),
+    }
+    assert np.allclose(state["observation"], expected_state["observation"])
+    assert np.allclose(state["achieved_goal"], expected_state["achieved_goal"])
+    assert np.allclose(state["desired_goal"], expected_state["desired_goal"])
 
 
 def test_seed_push():
@@ -39,9 +43,35 @@ def test_seed_push():
     for action in actions:
         state, _, _, _ = env.step(action)
     # check somes values from the final state
-    assert round(state["observation"][2], 8) == 0.07347989
-    assert round(state["achieved_goal"][1], 8) == 0.06545985
-    assert round(state["desired_goal"][0], 8) == 0.04850899
+    expected_state = {
+        "observation": np.array(
+            [
+                -2.42257369e-02,
+                8.08462671e-03,
+                7.34798944e-02,
+                -3.25473688e-01,
+                -1.05371741e-01,
+                1.91825476e-01,
+                -5.56536943e-02,
+                6.54598493e-02,
+                1.99899166e-02,
+                -3.34587545e-09,
+                -4.16869812e-06,
+                3.49416336e-05,
+                3.62493642e-06,
+                -2.78826608e-09,
+                3.62788052e-06,
+                1.39157062e-07,
+                1.81247759e-04,
+                -1.52381823e-10,
+            ]
+        ),
+        "achieved_goal": np.array([-0.05565369, 0.06545985, 0.01998992]),
+        "desired_goal": np.array([0.04850899, -0.04495698, 0.02]),
+    }
+    assert np.allclose(state["observation"], expected_state["observation"])
+    assert np.allclose(state["achieved_goal"], expected_state["achieved_goal"])
+    assert np.allclose(state["desired_goal"], expected_state["desired_goal"])
 
 
 def test_seed_slide():
@@ -59,9 +89,35 @@ def test_seed_slide():
     for action in actions:
         state, _, _, _ = env.step(action)
     # check somes values from the final state
-    assert round(state["observation"][2], 8) == 0.02201877
-    assert round(state["achieved_goal"][1], 8) == 0.02719425
-    assert round(state["desired_goal"][0], 8) == 0.45183363
+    expected_state = {
+        "observation": np.array(
+            [
+                -9.53984287e-04,
+                -4.27160227e-02,
+                2.20187671e-02,
+                -1.05556234e00,
+                -2.46236634e-01,
+                -7.07535426e-01,
+                6.15474529e-02,
+                2.71942454e-02,
+                1.49899397e-02,
+                -9.25502719e-06,
+                -1.29036812e-05,
+                1.10692239e-03,
+                8.88449868e-06,
+                -3.99026332e-06,
+                3.21412926e-06,
+                2.57145412e-04,
+                5.90577407e-04,
+                -2.52982825e-06,
+            ]
+        ),
+        "achieved_goal": np.array([0.06154745, 0.02719425, 0.01498994]),
+        "desired_goal": np.array([0.45183363, 0.04100129, 0.03]),
+    }
+    assert np.allclose(state["observation"], expected_state["observation"])
+    assert np.allclose(state["achieved_goal"], expected_state["achieved_goal"])
+    assert np.allclose(state["desired_goal"], expected_state["desired_goal"])
 
 
 def test_seed_pick_and_place():
@@ -78,10 +134,36 @@ def test_seed_pick_and_place():
     ]
     for action in actions:
         state, _, _, _ = env.step(action)
-    # check somes values from the final state
-    assert round(state["observation"][2], 8) == 0.09952821
-    assert round(state["achieved_goal"][1], 8) == -0.04404695
-    assert round(state["desired_goal"][0], 8) == -0.07605634
+    expected_state = {
+        "observation": np.array(
+            [
+                2.80928255e-02,
+                8.27207566e-03,
+                9.95282077e-02,
+                -1.56790883e-02,
+                1.45471370e-01,
+                -1.80584228e-01,
+                8.67016959e-03,
+                8.22368084e-02,
+                -4.40469502e-02,
+                1.99894128e-02,
+                4.48807556e-06,
+                -3.66759335e-05,
+                -2.32015619e-05,
+                -6.61656071e-07,
+                1.09536696e-05,
+                -1.21293651e-09,
+                7.80705244e-11,
+                -6.06878665e-08,
+                -9.66072195e-05,
+            ]
+        ),
+        "achieved_goal": np.array([0.08223681, -0.04404695, 0.01998941]),
+        "desired_goal": np.array([-0.07605634, 0.02172643, 0.20853403]),
+    }
+    assert np.allclose(state["observation"], expected_state["observation"])
+    assert np.allclose(state["achieved_goal"], expected_state["achieved_goal"])
+    assert np.allclose(state["desired_goal"], expected_state["desired_goal"])
 
 
 def test_seed_stack():
@@ -99,6 +181,45 @@ def test_seed_stack():
     for action in actions:
         state, _, _, _ = env.step(action)
     # check somes values from the final state
-    assert round(state["observation"][2], 8) == 0.03127798
-    assert round(state["achieved_goal"][1], 8) == -0.01094519
-    assert round(state["desired_goal"][0], 8) == 0.0533386
+    expected_state = {
+        "observation": np.array(
+            [
+                5.40996189e-02,
+                4.59357277e-02,
+                3.12779773e-02,
+                -3.42651042e-01,
+                -1.13286196e00,
+                -9.92534442e-01,
+                1.14918990e-03,
+                6.52296198e-02,
+                -1.09451851e-02,
+                1.99899066e-02,
+                -1.55383507e-08,
+                -4.68830546e-06,
+                -1.86308753e-05,
+                4.01648869e-06,
+                2.50407832e-08,
+                3.99006056e-06,
+                4.18980251e-09,
+                1.99501357e-04,
+                -1.79175223e-06,
+                9.90341818e-02,
+                9.23175415e-02,
+                1.99889895e-02,
+                6.74546584e-06,
+                -1.10591593e-05,
+                -1.03259781e-05,
+                9.45574685e-06,
+                6.04001409e-06,
+                4.39861731e-05,
+                -3.00748088e-04,
+                4.71467318e-04,
+                -1.78927334e-06,
+            ]
+        ),
+        "achieved_goal": np.array([0.06522962, -0.01094519, 0.01998991, 0.09903418, 0.09231754, 0.01998899]),
+        "desired_goal": np.array([0.0533386, 0.04649341, 0.02, 0.0533386, 0.04649341, 0.06]),
+    }
+    assert np.allclose(state["observation"], expected_state["observation"])
+    assert np.allclose(state["achieved_goal"], expected_state["achieved_goal"])
+    assert np.allclose(state["desired_goal"], expected_state["desired_goal"])
