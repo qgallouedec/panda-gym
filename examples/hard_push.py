445b17611e49d9bac02632468567faf45832929e
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2021-10-19 14:05:34 +0200

    add some examples

--- examples/hard_push.py
+++ examples/hard_push.py
@@ -3,8 +3,9 @@ import panda_gym
 import numpy as np
 
 env = gym.make("PandaPush-v2", render=True)
-
 obs = env.reset()
+
+# place behind
 for _ in range(10):
     desired_position = obs["achieved_goal"][0:3] - np.array([0.1, 0.0, 0.0])
     action = 5.0 * (desired_position - obs["observation"][0:3])
---
+++ examples/pick_and_place_joints.py
@@ -0,0 +1,12 @@
+import gym
+import panda_gym
+
+env = gym.make("PandaPickAndPlaceJoints-v2", render=True)
+
+obs = env.reset()
+done = False
+while not done:
+    action = env.action_space.sample()  # random action
+    obs, reward, done, info = env.step(action)
+    env.render()
+env.close()
