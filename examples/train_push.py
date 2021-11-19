b4705467c79d5ac60f4f23aa1a05514f5c60cf0d
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2021-11-19 11:16:42 +0100

    version 2.0.0a0

--- examples/train_push.py
+++ examples/train_push.py
@@ -2,8 +2,8 @@ import gym
 import panda_gym
 from stable_baselines3 import DDPG, HerReplayBuffer
 
-env = gym.make("PandaReach-v2")
+env = gym.make("PandaPush-v2")
 
 model = DDPG(policy="MultiInputPolicy", env=env, replay_buffer_class=HerReplayBuffer, verbose=1)
 
-model.learn(total_timesteps=10000)
+model.learn(total_timesteps=100000)
