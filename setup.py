d8aa7fc0cccc9efdc4acba388058a8cfe8c1c494
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-02-26 14:57:07 +0100

    panda-gym not compatible with python3.6 anymore

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     },
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
