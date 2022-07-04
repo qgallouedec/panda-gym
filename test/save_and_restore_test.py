c5fdc0f7d058738fbf2722eff9c2b5d7c7325207
Louis Qin <qyl0509@icloud.com>
2022-07-04 14:54:45 -0700

    Added unit test for remove state.

--- test/save_and_restore_test.py
+++ test/save_and_restore_test.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pybullet
+import pytest
 
 from panda_gym.envs.panda_tasks.panda_reach import PandaReachEnv
 
@@ -13,4 +15,10 @@ def test_save_and_restore_state():
     obs_restored = env.restore_state(state_id)
     assert(np.all(obs_original["achieved_goal"] == obs_restored["achieved_goal"]))
     assert(np.all(obs_original["observation"] == obs_restored["observation"]))
-    
+   
+def test_remove_state():
+    env = PandaReachEnv()
+    env.reset()
+    state_id = env.save_state()
+    with pytest.raises(pybullet.error):
+        env.remove_state(state_id)
