58bd0d12e527dff8508a7f5ccb65038b2c3646ae
Louis Qin <qyl0509@icloud.com>
2022-07-05 00:45:31 -0700

    Test for desired goal consistency during state saving and restoring.

--- test/save_and_restore_test.py
+++ test/save_and_restore_test.py
@@ -2,25 +2,31 @@ import numpy as np
 import pybullet
 import pytest
 
-from panda_gym.envs.panda_tasks.panda_reach import PandaReachEnv
+import gym
+import panda_gym
 
 
 def test_save_and_restore_state():
-    env = PandaReachEnv()
-    obs_original = env.reset()
+    env = gym.make("PandaReach-v2")
+    env.reset()
+
     state_id = env.save_state()
 
-    a = env.action_space.sample()
-    env.step(a)
+    # Perform the action
+    action = env.action_space.sample()
+    next_obs1, reward, done, info = env.step(action)
 
+    # Restore and perform the same action
     env.restore_state(state_id)
-    obs_restored = env._get_obs()
-    assert np.all(obs_original["achieved_goal"] == obs_restored["achieved_goal"])
-    assert np.all(obs_original["observation"] == obs_restored["observation"])
+    next_obs2, reward, done, info = env.step(action)
 
-
+    # The observations in both cases should be equals
+    assert(np.all(next_obs1["achieved_goal"] == next_obs2["achieved_goal"]))
+    assert(np.all(next_obs1["observation"] == next_obs2["observation"]))
+    assert(np.all(next_obs1["desired_goal"] == next_obs2["desired_goal"]))
+   
 def test_remove_state():
-    env = PandaReachEnv()
+    env = gym.make("PandaReach-v2")
     env.reset()
     state_id = env.save_state()
     env.remove_state(state_id)
