f57d53a0ef8c958ac1ff31ae111dffeb9cc143f2
Louis Qin <qyl0509@icloud.com>
2022-07-05 00:02:34 -0700

    Fixed save and restore test logic.

--- test/save_and_restore_test.py
+++ test/save_and_restore_test.py
@@ -12,7 +12,8 @@ def test_save_and_restore_state():
     a = env.action_space.sample()
     env.step(a)
 
-    obs_restored = env.restore_state(state_id)
+    env.restore_state(state_id)
+    obs_restored = env._get_obs()
     assert(np.all(obs_original["achieved_goal"] == obs_restored["achieved_goal"]))
     assert(np.all(obs_original["observation"] == obs_restored["observation"]))
    
@@ -20,5 +21,6 @@ def test_remove_state():
     env = PandaReachEnv()
     env.reset()
     state_id = env.save_state()
+    env.remove_state(state_id)
     with pytest.raises(pybullet.error):
-        env.remove_state(state_id)
+        env.restore_state(state_id)
