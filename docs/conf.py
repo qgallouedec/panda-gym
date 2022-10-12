a9c02379fad541a3e02599dc50a6cf522158f958
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-10-12 16:39:39 +0200

    Allow return tuple documentation

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ release = "v2.0.4"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
+napoleon_custom_sections = [('Returns', 'params_style')]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
