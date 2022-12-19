631e8a4f22021ced69538a13357042dbc0a980a5
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-12-19 11:27:00 +0100

    Update examples

--- examples/rgb_rendering.py
+++ examples/rgb_rendering.py
@@ -3,21 +3,21 @@ from numpngw import write_apng  # pip install numpngw
 
 import panda_gym
 
-env = gym.make("PandaStack-v3", render=True)
+env = gym.make("PandaStack-v3", render_mode="rgb_array")
 images = []
 
 
 observation, info = env.reset()
-images.append(env.render("rgb_array"))
+images.append(env.render())
 
 for _ in range(1000):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
-    images.append(env.render("rgb_array"))
+    images.append(env.render())
 
     if terminated or truncated:
         observation, info = env.reset()
-        images.append(env.render("rgb_array"))
+        images.append(env.render())
 
 env.close()
 
