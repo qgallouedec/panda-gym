af61d14d996779cb93bbfeedbde554c196574fb2
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-01-23 13:08:59 +0100

    add test

---
+++ test/render_test.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+from gymnasium.wrappers import PixelObservationWrapper
+
+import panda_gym
+
+
+def test_pixel_observation_wrapper():
+    env = gym.make("PandaReach-v3", render_mode="rgb_array")
+    env = PixelObservationWrapper(env)
+
+    observation, _ = env.reset()
+    assert observation["pixels"].shape == (480, 720, 4)
+
+    for _ in range(100):
+        action = env.action_space.sample()  # random action
+        observation, _, terminated, truncated, _ = env.step(action)
+        assert observation["pixels"].shape == (480, 720, 4)
+        if terminated or truncated:
+            observation, _ = env.reset()
+
+    env.close()
