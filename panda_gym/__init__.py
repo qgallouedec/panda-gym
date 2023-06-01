1efe6abd4891af7bd8306055aee58bf1f00ace33
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-06-01 10:17:08 +0200

    expose env_ids and 3ple loop registration

--- panda_gym/__init__.py
+++ panda_gym/__init__.py
@@ -5,50 +5,20 @@ from gymnasium.envs.registration import register
 with open(os.path.join(os.path.dirname(__file__), "version.txt"), "r") as file_handler:
     __version__ = file_handler.read().strip()
 
-for reward_type in ["sparse", "dense"]:
-    for control_type in ["ee", "joints"]:
-        reward_suffix = "Dense" if reward_type == "dense" else ""
-        control_suffix = "Joints" if control_type == "joints" else ""
-        kwargs = {"reward_type": reward_type, "control_type": control_type}
+ENV_IDS = []
 
-        register(
-            id="PandaReach{}{}-v3".format(control_suffix, reward_suffix),
-            entry_point="panda_gym.envs:PandaReachEnv",
-            kwargs=kwargs,
-            max_episode_steps=50,
-        )
+for task in ["Reach", "Slide", "Push", "PickAndPlace", "Stack", "Flip"]:
+    for reward_type in ["sparse", "dense"]:
+        for control_type in ["ee", "joints"]:
+            reward_suffix = "Dense" if reward_type == "dense" else ""
+            control_suffix = "Joints" if control_type == "joints" else ""
+            env_id = f"Panda{task}{control_suffix}{reward_suffix}-v3"
 
-        register(
-            id="PandaPush{}{}-v3".format(control_suffix, reward_suffix),
-            entry_point="panda_gym.envs:PandaPushEnv",
-            kwargs=kwargs,
-            max_episode_steps=50,
-        )
+            register(
+                id=env_id,
+                entry_point=f"panda_gym.envs:Panda{task}Env",
+                kwargs={"reward_type": reward_type, "control_type": control_type},
+                max_episode_steps=100 if task == "Stack" else 50,
+            )
 
-        register(
-            id="PandaSlide{}{}-v3".format(control_suffix, reward_suffix),
-            entry_point="panda_gym.envs:PandaSlideEnv",
-            kwargs=kwargs,
-            max_episode_steps=50,
-        )
-
-        register(
-            id="PandaPickAndPlace{}{}-v3".format(control_suffix, reward_suffix),
-            entry_point="panda_gym.envs:PandaPickAndPlaceEnv",
-            kwargs=kwargs,
-            max_episode_steps=50,
-        )
-
-        register(
-            id="PandaStack{}{}-v3".format(control_suffix, reward_suffix),
-            entry_point="panda_gym.envs:PandaStackEnv",
-            kwargs=kwargs,
-            max_episode_steps=100,
-        )
-
-        register(
-            id="PandaFlip{}{}-v3".format(control_suffix, reward_suffix),
-            entry_point="panda_gym.envs:PandaFlipEnv",
-            kwargs=kwargs,
-            max_episode_steps=50,
-        )
+            ENV_IDS.append(env_id)
