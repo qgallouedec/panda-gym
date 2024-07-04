d98e5862dc7e4f72ffd727758b78142e3fd08cc2
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-07-04 15:12:35 +0200

    better seed

--- test/save_and_restore_test.py
+++ test/save_and_restore_test.py
@@ -10,7 +10,7 @@ def test_save_and_restore_state():
     env = gym.make("PandaReach-v3")
     env.reset()
 
-    state_id = env.save_state()
+    state_id = env.unwrapped.save_state()
 
     # Perform the action
     action = env.action_space.sample()
@@ -18,7 +18,7 @@ def test_save_and_restore_state():
 
     # Restore and perform the same action
     env.reset()
-    env.restore_state(state_id)
+    env.unwrapped.restore_state(state_id)
     observation2, _, _, _, _ = env.step(action)
 
     # The observations in both cases should be equals
@@ -30,7 +30,7 @@ def test_save_and_restore_state():
 def test_remove_state():
     env = gym.make("PandaReach-v3")
     env.reset()
-    state_id = env.save_state()
-    env.remove_state(state_id)
+    state_id = env.unwrapped.save_state()
+    env.unwrapped.remove_state(state_id)
     with pytest.raises(pybullet.error):
-        env.restore_state(state_id)
+        env.unwrapped.restore_state(state_id)
